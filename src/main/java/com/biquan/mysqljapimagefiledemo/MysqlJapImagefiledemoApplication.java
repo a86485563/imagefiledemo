dda909404cc77c1bf7026bae20dfc5de910654ef
biquanyan001 <biquanyan@fareastone.com.tw>
2019-12-23 11:17:10 +0800

    first commit

---
+++ src/main/java/com/biquan/mysqljapimagefiledemo/MysqlJapImagefiledemoApplication.java
@@ -0,0 +1,13 @@
+package com.biquan.mysqljapimagefiledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlJapImagefiledemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlJapImagefiledemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biquan/mysqljapimagefiledemo/MysqlJapImagefiledemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biquan.mysqljapimagefiledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlJapImagefiledemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
